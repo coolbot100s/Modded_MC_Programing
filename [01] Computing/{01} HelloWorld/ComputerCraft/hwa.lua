ab552a7e8b4b37c7fdb955edad0437b31cc4cc79
coolbot100s <76798835+coolbot100s@users.noreply.github.com>
2024-04-23 11:55:35 -0700

    Update hwa.lua

--- [01] Computing/{01} HelloWorld/ComputerCraft/hwa.lua
+++ [01] Computing/{01} HelloWorld/ComputerCraft/hwa.lua
@@ -2,5 +2,5 @@
 -- By coolbot100s
 -- MC version 1.20.1, CC:Tweaked version 1.110.2
 
--- Print "Hello, world!" to the terminal when the program is ran
+-- Print "Hello, world!" to the terminal when the program is run
 print("Hello, world!")
